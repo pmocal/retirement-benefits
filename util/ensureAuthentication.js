1812dba61874c180314f4f246b755e20082171a7
Parthiv Mohan <parthiv.mohan@gmail.com>
2020-01-29 16:51:31 -0500

    #15

--- util/ensureAuthentication.js
+++ util/ensureAuthentication.js
@@ -1,14 +1,14 @@
 exports.ensureAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
-  	return next();
-  } else {
-  	res.sendStatus(401);
-  }
+	if (req.isAuthenticated()) {
+		return next();
+	} else {
+		res.sendStatus(401);
+	}
 }
 
 exports.noCache = (req, res, next) => {
-  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-  res.header('Expires', '-1');
-  res.header('Pragma', 'no-cache');
-  next();
+	res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+	res.header('Expires', '-1');
+	res.header('Pragma', 'no-cache');
+	next();
 }
