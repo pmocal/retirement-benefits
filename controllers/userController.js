c1c50a196568e71df2031b71c0de77481b776ded
Parthiv Mohan <parthiv.mohan@gmail.com>
2020-01-30 00:40:48 -0500

    https://github.com/pmocal/retirement-benefits/issues/1

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,14 +23,27 @@ exports.sign_up_get = (req, res) => res.render("sign_up_form", { title: "Sign up
 
 exports.sign_up_post = [
 	// Validate fields.
-	check('fullname', 'Full name must not be empty.').isLength({ min: 1 }).trim(),
+	check('last_name', 'Last name must not be empty.').isLength({ min: 1 }).trim(),
+	check('first_name', 'First name must not be empty.').isLength({ min: 1 }).trim(),
+	check('address', 'Address must not be empty.').isLength({ min: 1 }).trim(),
+	check('city', 'City must not be empty.').isLength({ min: 1 }).trim(),
+	check('state', 'State must not be empty.').isLength({ min: 1 }).trim(),
 	check('ssn', 'SSN must be at least 9 digits').isLength({ min: 9 }).trim(),
+	check('dob', 'Invalid date of birth').optional({ checkFalsy: true }).isISO8601(),
+	check('phone_number', 'Phone number must be at least 9 digits').isLength({ min: 9 }).trim(),
 	check('username', 'Username must not be empty.').isLength({ min: 1 }).trim(),
 	check('password', 'Password must not be empty').isLength({ min: 1 }).trim(),
 	check('passwordConfirmation').exists(),
 	// Sanitize fields (using wildcard).
-	sanitizeBody('fullname').escape(),
-	sanitizeBody('SSN').escape(),
+	sanitizeBody('last_name').escape(),
+	sanitizeBody('first_name').escape(),
+	sanitizeBody('address').escape(),
+	sanitizeBody('city').escape(),
+	sanitizeBody('state').escape(),
+	sanitizeBody('ssn').escape(),
+	sanitizeBody('sex').escape(),
+	sanitizeBody('dob').toDate(),
+	sanitizeBody('phone_number').escape(),
 	sanitizeBody('username').escape(),
 	sanitizeBody('password').escape(),
 	sanitizeBody('passwordConfirmation').escape(),
@@ -40,8 +53,13 @@ exports.sign_up_post = [
 	(req, res, next) => {
 		// Extract the validation errors from a request.
 		var user = new User({
-			username: req.body.username,
-			fullname: req.body.fullname
+			last_name: req.body.last_name,
+			first_name: req.body.first_name,
+			address: req.body.address,
+			city: req.body.city,
+			state: req.body.state,
+			phone_number: req.body.phone_number,
+			username: req.body.username
 		})
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
@@ -54,10 +72,19 @@ exports.sign_up_post = [
 			bcrypt.genSalt(10, function(err, salt) {
 				bcrypt.hash(req.body.password, salt, function(err, hash) {
 					user = new User({
-						username: req.body.username,
-						fullname: req.body.fullname,
+						last_name: req.body.last_name,
+						first_name: req.body.first_name,
+						address: req.body.address,
+						city: req.body.city,
+						state: req.body.state,
 						ssn: req.body.ssn,
+						sex: req.body.sex,
+						dob: (req.body.dob == null) ? req.body.dob :
+							new Date(req.body.dob.toISOString().replace(/-/g, '\/').replace(/T.+/, '')),
+						phone_number: req.body.phone_number,
+						username: req.body.username,
 						password: hash,
+						is_admin: false
 					}).save(err => {
 						if (err) { 
 							return next(err);
--- models/user.js
+++ models/user.js
@@ -1,14 +1,40 @@
 var mongoose = require('mongoose');
+const moment = require('moment-timezone');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema(
 	{
+		last_name: { type: String, required: true },
+		first_name: { type: String, required: true },
+		address: { type: String, required: true },
+		city: { type: String, required: true },
+		state: { type: String, required: true },
+		ssn: { type: String, required: true },
+		sex: {type: String, required: true, enum: ['male', 'female', 'other']},
+		dob: { type: Date, required: true },
+		phone_number: { type: String, required: true },
 		username: { type: String, required: true },
 		password: { type: String, required: true },
-		fullname: { type: String, required: true },
-		ssn: { type: String, required: true },
-		isAdmin: false
+		is_admin: false
 	}
 )
+
+UserSchema
+.virtual('dob_formatted')
+.get(function () {
+	return moment(this.dob).format('YYYYMMDD');
+});
+
+UserSchema
+.virtual('phone_number_formatted')
+.get(function () {
+  return this.phone_number.slice(0,3) + '-' + this.phone_number.slice(3,6) + '-' + this.phone_number.slice(6,10);
+});
+
+UserSchema
+.virtual('ssn_formatted')
+.get(function () {
+  return this.ssn.slice(0,3) + '-' + this.ssn.slice(3,5) + '-' + this.ssn.slice(5,9);
+});
 
 module.exports = mongoose.model('User', UserSchema);
